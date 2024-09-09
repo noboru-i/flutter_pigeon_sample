b5dd93499818e43f8251437d0b80fac39a3a78c0
noboru-i <ishikura.noboru@gmail.com>
2024-09-09 21:51:02 +0900

    format code

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,14 +3,14 @@ import Flutter
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-      
-      let controller: FlutterViewController = window?.rootViewController as! FlutterViewController
-      BatteryApiSetup.setUp(binaryMessenger: controller as! FlutterBinaryMessenger, api: BatteryApiImplementation())
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        GeneratedPluginRegistrant.register(with: self)
+        
+        let controller: FlutterViewController = window?.rootViewController as! FlutterViewController
+        BatteryApiSetup.setUp(binaryMessenger: controller as! FlutterBinaryMessenger, api: BatteryApiImplementation())
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
 }
